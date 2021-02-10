7f8fba65e2de9945d52b2360e9d1f6f1693620b6
Jlll1 <arghantentua@tutanota.com>
2021-02-10 21:30:55 +0100

    Added functional ISBN finder for PDF documents.

---
+++ src/Expelibrum/Expelibrum.Core/PDFUtils.cs
@@ -0,0 +1,111 @@
+﻿using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.Content;
+using PdfSharpCore.Pdf.Content.Objects;
+using PdfSharpCore.Pdf.IO;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Expelibrum.Core
+{
+    public class PDFUtils
+    {
+        private string _file;
+
+        public void OpenDocument(string fileName)
+        {
+            _file = fileName;
+        }
+
+        public string GetIsbn()
+        {
+            var substrings = GetText().Split(" ");
+
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                if (substrings[i].ToLowerInvariant().Equals("isbn"))
+                {
+                    return substrings[i + 1];
+                }
+            }
+
+            throw new InvalidOperationException("Isbn not found in the document");
+        }
+
+        private string GetText()
+        {
+            using (var _document = PdfReader.Open(_file, PdfDocumentOpenMode.ReadOnly))
+            {
+                var result = new StringBuilder();
+                foreach (var page in _document.Pages.OfType<PdfPage>())
+                {
+                    ExtractText(ContentReader.ReadContent(page), result);
+                    result.AppendLine();
+                }
+
+                return result.ToString();
+            }
+        }
+
+        #region CObject Visitor
+        private static void ExtractText(CObject obj, StringBuilder target)
+        {
+            if (obj is CArray)
+                ExtractText((CArray)obj, target);
+            else if (obj is CComment)
+                ExtractText((CComment)obj, target);
+            else if (obj is CInteger)
+                ExtractText((CInteger)obj, target);
+            else if (obj is CName)
+                ExtractText((CName)obj, target);
+            else if (obj is CNumber)
+                ExtractText((CNumber)obj, target);
+            else if (obj is COperator)
+                ExtractText((COperator)obj, target);
+            else if (obj is CReal)
+                ExtractText((CReal)obj, target);
+            else if (obj is CSequence)
+                ExtractText((CSequence)obj, target);
+            else if (obj is CString)
+                ExtractText((CString)obj, target);
+            else
+                throw new NotImplementedException(obj.GetType().AssemblyQualifiedName);
+        }
+
+        private static void ExtractText(CArray obj, StringBuilder target)
+        {
+            foreach (var element in obj)
+            {
+                ExtractText(element, target);
+            }
+        }
+        private static void ExtractText(CComment obj, StringBuilder target) { /* nothing */ }
+        private static void ExtractText(CInteger obj, StringBuilder target) { /* nothing */ }
+        private static void ExtractText(CName obj, StringBuilder target) { /* nothing */ }
+        private static void ExtractText(CNumber obj, StringBuilder target) { /* nothing */ }
+        private static void ExtractText(COperator obj, StringBuilder target)
+        {
+            if (obj.OpCode.OpCodeName == OpCodeName.Tj || obj.OpCode.OpCodeName == OpCodeName.TJ)
+            {
+                foreach (var element in obj.Operands)
+                {
+                    ExtractText(element, target);
+                }
+                target.Append(" ");
+            }
+        }
+        private static void ExtractText(CReal obj, StringBuilder target) { /* nothing */ }
+        private static void ExtractText(CSequence obj, StringBuilder target)
+        {
+            foreach (var element in obj)
+            {
+                ExtractText(element, target);
+            }
+        }
+        private static void ExtractText(CString obj, StringBuilder target)
+        {
+            target.Append(obj.Value);
+        }
+        #endregion
+    }
+}
