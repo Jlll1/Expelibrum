2088b65760f8260c747c5d1efbfac4142ed71fdd
Jlll1 <arghantentua@tutanota.com>
2021-02-11 03:55:44 +0100

    Added a Open Library helper class

---
+++ src/Expelibrum/Expelibrum.Core/IsbnHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Expelibrum.Core
+{
+    public class IsbnHelper
+    {
+        private const string ENDPOINT = @"https://openlibrary.org/api/books?bibkeys={0}&jscmd=data&format=json";
+
+        public static async Task<Book> GetBookInfo(string isbn)
+        {
+            string url = string.Format(ENDPOINT, isbn);
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<Book>(json);
+            }
+        }
+    }
+}
