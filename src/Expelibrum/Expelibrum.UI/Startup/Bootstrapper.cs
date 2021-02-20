1688cdfbcf6535d737a28474338d4f0419b5bced
Jlll1 <arghantentua@tutanota.com>
2021-02-20 21:26:27 +0100

    Added DI Container

---
+++ src/Expelibrum/Expelibrum.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Expelibrum.Services;
+
+namespace Expelibrum.UI.Startup
+{
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<OpenLibraryApiService>().As<IIsbnService>();
+
+
+            return builder.Build();
+        }
+    }
+}
