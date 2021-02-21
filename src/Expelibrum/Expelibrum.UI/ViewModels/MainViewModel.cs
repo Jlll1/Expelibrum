22c3d70aa806028c80485c7d06c373563a6ac804
Jlll1 <arghantentua@tutanota.com>
2021-02-21 17:24:25 +0100

    Hooked up the viewmodels

--- src/Expelibrum/Expelibrum.UI/ViewModels/MainViewModel.cs
+++ src/Expelibrum/Expelibrum.UI/ViewModels/MainViewModel.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Expelibrum.UI.ViewModels
+﻿namespace Expelibrum.UI.ViewModels
 {
     public class MainViewModel
     {
+        public IDirectoryViewModel DirectoryViewModel { get; }
+
+        public MainViewModel(IDirectoryViewModel directoryViewModel)
+        {
+            DirectoryViewModel = directoryViewModel;
+        }
     }
 }
