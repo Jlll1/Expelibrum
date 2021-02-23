68c4187698d6b243ea8893f03f99f12533e57b2f
Jlll1 <arghantentua@tutanota.com>
2021-02-23 19:43:07 +0100

    Extracted DirectorySettingsViewModel from ProcessViewModel

---
+++ src/Expelibrum/Expelibrum.UI/ViewModels/IDirectorySettingsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace Expelibrum.UI.ViewModels
+{
+    public interface IDirectorySettingsViewModel
+    {
+        ICommand ChangeOriginDirectoryPathCommand { get; }
+        ICommand ChangeTargetDirectoryPathCommand { get; }
+        bool IncludeSubdirectories { get; set; }
+        string OriginDirectoryPath { get; set; }
+        string TargetDirectoryPath { get; set; }
+    }
+}
