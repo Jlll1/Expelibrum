bce2182efaae4d4fad8ad086372b82162349394a
Jlll1 <arghantentua@tutanota.com>
2021-02-23 19:58:27 +0100

    Adapted ProcessViewModel to accomodate extraction of the DirectorySettingsViewModel

--- src/Expelibrum/Expelibrum.UI/ViewModels/ProcessViewModel.cs
+++ src/Expelibrum/Expelibrum.UI/ViewModels/ProcessViewModel.cs
@@ -13,71 +13,28 @@ namespace Expelibrum.UI.ViewModels
 
         #region fields
 
-        private IFolderBrowserDialog _folderBrowser;
         private IPDFUtils _pdfUtils;
         private IIsbnService _isbnService;
 
-        private string _originDirectoryPath;
-        private string _targetDirectoryPath;
-        private bool _includeSubdirectories = true;
+        private IDirectorySettingsViewModel _directorySettings;
 
         #endregion
 
         #region properties
 
-        public string OriginDirectoryPath
-        {
-            get => _originDirectoryPath;
-            set
-            {
-                _originDirectoryPath = value;
-                OnPropertyChanged();
-            }
-        }
-        public string TargetDirectoryPath
-        {
-            get => _targetDirectoryPath;
-            set
-            {
-                _targetDirectoryPath = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public bool IncludeSubdirectories
-        {
-            get => _includeSubdirectories;
-            set 
-            { 
-                _includeSubdirectories = value;
-                OnPropertyChanged();
-            }
-        }
+        public IDirectorySettingsViewModel DirectorySettings { get; }
 
         #endregion
 
         #region commands
-
-        public ICommand ChangeOriginDirectoryPathCommand { get; }
-        public ICommand ChangeTargetDirectoryPathCommand { get; }
         public ICommand ProcessFilesCommand { get; }
 
         #region commandmethods
 
-        private void OnChangeOriginDirectoryPath(object param)
-        {
-            OriginDirectoryPath = _folderBrowser.GetDirectoryPathDialog();
-        }
-
-        private void OnChangeTargetDirectoryPath(object param)
-        {
-            TargetDirectoryPath = _folderBrowser.GetDirectoryPathDialog();
-        }
-
         private async void OnProcessFiles(object param)
         {
-            var directory = new DirectoryInfo(OriginDirectoryPath);
-            var searchOption = (SearchOption)Convert.ToInt32(IncludeSubdirectories);
+            var directory = new DirectoryInfo(DirectorySettings.OriginDirectoryPath);
+            var searchOption = (SearchOption)Convert.ToInt32(DirectorySettings.IncludeSubdirectories);
 
             foreach (var file in directory.GetFiles("*.pdf", searchOption))
             {
@@ -85,11 +42,11 @@ namespace Expelibrum.UI.ViewModels
                 {
                     Book book = await GetBookFromFileAsync(file.FullName);
                     string newTitle = book.title + ".pdf";
-                    Directory.Move(file.FullName, Path.Combine(TargetDirectoryPath, newTitle));
+                    Directory.Move(file.FullName, Path.Combine(DirectorySettings.TargetDirectoryPath, newTitle));
                 }
                 catch (InvalidOperationException)
                 {
-                    Directory.Move(file.FullName, Path.Combine(TargetDirectoryPath, file.Name));
+                    Directory.Move(file.FullName, Path.Combine(DirectorySettings.TargetDirectoryPath, file.Name));
                 }
             }
         }
@@ -99,15 +56,15 @@ namespace Expelibrum.UI.ViewModels
         #endregion
 
         #region constructors
-        public ProcessViewModel(IFolderBrowserDialog folderBrowser,
-            IPDFUtils pdfUtils, IIsbnService isbnService)
+        public ProcessViewModel(IDirectorySettingsViewModel directorySettingsViewModel,
+            IPDFUtils pdfUtils,
+            IIsbnService isbnService)
         {
-            _folderBrowser = folderBrowser;
             _pdfUtils = pdfUtils;
             _isbnService = isbnService;
 
-            ChangeOriginDirectoryPathCommand = new RelayCommand(OnChangeOriginDirectoryPath);
-            ChangeTargetDirectoryPathCommand = new RelayCommand(OnChangeTargetDirectoryPath);
+            DirectorySettings = directorySettingsViewModel;
+
             ProcessFilesCommand = new RelayCommand(OnProcessFiles);
 
         }
@@ -118,8 +75,8 @@ namespace Expelibrum.UI.ViewModels
 
         private async Task<Book> GetBookFromFileAsync(string file)
         {
-                string isbn = _pdfUtils.GetIsbn(file);
-                return await _isbnService.GetBookFromIsbn(isbn);
+            string isbn = _pdfUtils.GetIsbn(file);
+            return await _isbnService.GetBookFromIsbn(isbn);
         }
 
         #endregion
