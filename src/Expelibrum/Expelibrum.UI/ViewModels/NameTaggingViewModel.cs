a8ab9df8d0796fc0757a1186b816d08b80e63122
Jlll1 <arghantentua@tutanota.com>
2021-02-27 11:51:08 +0100

    Add tags to the NameTaggingViewModel

--- src/Expelibrum/Expelibrum.UI/ViewModels/NameTaggingViewModel.cs
+++ src/Expelibrum/Expelibrum.UI/ViewModels/NameTaggingViewModel.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Expelibrum.Model;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Expelibrum.UI.ViewModels
 {
-    class NameTaggingViewModel
+    public class NameTaggingViewModel
     {
+        public List<Tag> Tags { get; }
+
+        public NameTaggingViewModel()
+        {
+            Tags = new List<Tag>
+            {
+                new Tag("Title", "title"),
+                new Tag("Author", "authors"),
+                new Tag("PageCount", "number_of_pages"),
+                new Tag("Publisher", "publishers"),
+                new Tag("PublishDate", "publish_date"),
+                new Tag("Subject", "subjects")
+            };
+        }
     }
 }
