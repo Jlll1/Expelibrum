daf037435a4bd7076de4ecda4fec1e2e30ef16f1
Jlll1 <arghantentua@tutanota.com>
2021-03-04 08:26:41 +0100

    Make MainWindow a MetroWindow

--- src/Expelibrum/Expelibrum.UI/MainWindow.xaml.cs
+++ src/Expelibrum/Expelibrum.UI/MainWindow.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using Expelibrum.UI.ViewModels;
-using System.Windows;
+using MahApps.Metro.Controls;
 
 namespace Expelibrum.UI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         private MainViewModel _viewModel;
 
