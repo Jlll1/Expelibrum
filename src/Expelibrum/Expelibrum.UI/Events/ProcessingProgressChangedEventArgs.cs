adaf2b2c9ca2641af43e3db68b6dd83dd46655ba
Jlll1 <71319302+Jlll1@users.noreply.github.com>
2021-03-05 11:35:05 +0100

    Add Progress Indicator (Closes #41) (#45)
    * Add ProgressIndicator
    
    Add Events
    
    Connect ProgressIndicator with Events
    
    Connect Process with Events
    
    Clean up
    
    * Styling fix

---
+++ src/Expelibrum/Expelibrum.UI/Events/ProcessingProgressChangedEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Expelibrum.UI.Events
+{
+    public class ProcessingProgressChangedEventArgs : EventArgs
+    {
+        public int Completed;
+    }
+}
---
+++ src/Expelibrum/Expelibrum.UI/ViewModels/IProgressIndicatorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Expelibrum.UI.ViewModels
+{
+    public interface IProgressIndicatorViewModel
+    {
+        int Completed { get; set; }
+        Visibility Status { get; }
+        int Total { get; set; }
+    }
+}
